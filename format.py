bca6db4090a2617fdd1c3a483910519e701a9f37
Johana Rincon <41636531+johanarincon20141@users.noreply.github.com>
2024-06-14 23:09:22 -0500

    PY
    EJERCICIOS

---
+++ format.py
@@ -0,0 +1,3 @@
+chanchito = "feliz"
+a =   2
+b=   313
---
+++ intro.py
@@ -0,0 +1,9 @@
+
+"""Introduccion a python """
+
+print ("Hola mundo estoy aprendiendo a programar")
+print ("el weta" * 4)
+print("johana " * 4)
+print("laaaa")
+print("es una prueba de python")
+
